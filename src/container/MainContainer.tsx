a1291836f52c8fb12671ec7a8e211e4e43bb0133
cteyr123 <cteyr123@gmail.com>
2022-07-15 23:46:52 +0200

    margen tiitle add

--- src/container/MainContainer.tsx
+++ src/container/MainContainer.tsx
@@ -123,68 +123,66 @@ const MainContainer = () => {
         </Box>
       </Box>
 
-      <div className="container-table">
-        {isLoading ? (
-          <TableContainer sx={{ height: "65vh" }}>
-            <Table sx={{ minWidth: 0 }} aria-label="simple table">
-              <TableHead sx={{ width: "100%" }}>
-                <TableRow>
-                  <TableCell
-                    align="center"
-                    style={{ width: "100px", backgroundColor: "#2196f3" }}
-                  >
-                    ID
+      {isLoading ? (
+        <TableContainer sx={{}} className="container-table">
+          <Table sx={{ minWidth: 0 }} aria-label="simple table">
+            <TableHead sx={{ width: "100%" }}>
+              <TableRow>
+                <TableCell
+                  align="center"
+                  style={{ width: "100px", backgroundColor: "#2196f3" }}
+                >
+                  ID
+                </TableCell>
+                <TableCell
+                  align="center"
+                  style={{ width: "250px", backgroundColor: "#2196f3" }}
+                >
+                  Name
+                </TableCell>
+                <TableCell
+                  align="center"
+                  style={{ width: "250px", backgroundColor: "#2196f3" }}
+                >
+                  Status
+                </TableCell>
+                <TableCell
+                  align="center"
+                  style={{ width: "250px", backgroundColor: "#2196f3" }}
+                >
+                  Specie
+                </TableCell>
+                <TableCell
+                  align="center"
+                  style={{ width: "250px", backgroundColor: "#2196f3" }}
+                >
+                  Image
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody className="Table-Body">
+              {filterCharacters().map((aliens, index) => (
+                <TableRow
+                  key={aliens.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" align="center">
+                    {aliens.id}
                   </TableCell>
-                  <TableCell
-                    align="center"
-                    style={{ width: "250px", backgroundColor: "#2196f3" }}
-                  >
-                    Name
-                  </TableCell>
-                  <TableCell
-                    align="center"
-                    style={{ width: "250px", backgroundColor: "#2196f3" }}
-                  >
-                    Status
-                  </TableCell>
-                  <TableCell
-                    align="center"
-                    style={{ width: "250px", backgroundColor: "#2196f3" }}
-                  >
-                    Specie
-                  </TableCell>
-                  <TableCell
-                    align="center"
-                    style={{ width: "250px", backgroundColor: "#2196f3" }}
-                  >
-                    Image
+                  <TableCell align="center">{aliens.name}</TableCell>
+                  <TableCell align="center">{aliens.status}</TableCell>
+                  <TableCell align="center">{aliens.species}</TableCell>
+                  <TableCell align="center">
+                    <img src={aliens.image} alt="" style={{ height: 75 }} />
                   </TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody className="Table-Body">
-                {filterCharacters().map((aliens, index) => (
-                  <TableRow
-                    key={aliens.id}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell component="th" align="center">
-                      {aliens.id}
-                    </TableCell>
-                    <TableCell align="center">{aliens.name}</TableCell>
-                    <TableCell align="center">{aliens.status}</TableCell>
-                    <TableCell align="center">{aliens.species}</TableCell>
-                    <TableCell align="center">
-                      <img src={aliens.image} alt="" style={{ height: 75 }} />
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        ) : (
-          <DotSpinner size={80} speed={0.9} color="#1e88e5" />
-        )}
-      </div>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <DotSpinner size={80} speed={0.9} color="#1e88e5" />
+      )}
     </div>
   );
 };
